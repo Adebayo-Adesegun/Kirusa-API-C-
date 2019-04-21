7e151ad8e9a8a70363521ad86f2370d11554a503
Adebayo Adesegun Daniel <chamsdev@outlook.com>
2019-04-21 17:09:09 +0100

    Add comments to request object

--- KonnectAPIC/Request.cs
+++ KonnectAPIC/Request.cs
@@ -6,10 +6,15 @@ namespace KonnectAPIC
 {
     public class Request
     {
+        // Unique transaction id for the request
         public string id { get; set; }
+        //The sender phone number. If not specified, the default sender id is used.
         public string from { get; set; }
+        // The destination phone number, multiple phone numbers should be passed as a JSON array separated by comma.
         public List<string> to { get; set; }
+        // The string to mask the sender.
         public string sender_mask { get; set; }
+       // The full text of the message
         public string body { get; set; }
 
     }
